15b52d926c0a363a3f9cd69305ecc1996927377d
casey cole <caseycole589@gmail.com>
2018-05-20 20:03:47 -0400

    send the c book quote as json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! really It works!');
+// send resp as json resp.json(jsObject)
+router.get('/', (req, resp) => {
+	const message = {
+		"message": "Hello Wolrd!",
+		"quoteBy": "Dennis Ritchie"
+	};
+	// resp.send("string")
+	resp.json(message);
 });
 
 module.exports = router;
