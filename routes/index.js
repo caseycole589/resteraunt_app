603d11859c08ba182c32a854b867fd73c0b314f4
casey cole <caseycole589@gmail.com>
2018-05-20 20:13:40 -0400

    created a route to reverse a string

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,15 @@ router.get('/', (req, resp) => {
 		"quoteBy": "Dennis Ritchie"
 	};
 	// resp.send("string")
-	resp.json(message);
+	// resp.json(message);
+	// send back the query aka the echo server
+	resp.send(req.query);
 });
+
+router.get('/reverse/:string_to_reverse', (req, resp) => {
+	//split the string
+	const reversed_string = [...req.params.string_to_reverse].reverse().join('');
+	resp.send(reversed_string);
+})
 
 module.exports = router;
