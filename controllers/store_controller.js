1d3f8806c4c2f2fbc5f9bc99eec3b7fcfad7a98b
casey cole <caseycole589@gmail.com>
2018-06-09 21:00:29 -0400

    out put form json

--- controllers/store_controller.js
+++ controllers/store_controller.js
@@ -7,3 +7,7 @@ exports.addStore = (req, res) => {
 	// res.send('it works')
 	res.render('editStore', {title: 'Add Store'})
 }
+
+exports.createStore = (req, res) => {
+	res.json(req.body);
+}
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const store_controller = require('../controllers/store_controller')
 
 router.get('/', store_controller.homePage);
 router.get('/add', store_controller.addStore);
+router.post('/add', store_controller.createStore);
 
 module.exports = router;
