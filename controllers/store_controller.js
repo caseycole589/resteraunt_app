a3cbb0c6ad69c00e4246592c5cf37cb310070dec
Casey Cole <caseycole589@gmail.com>
2018-06-11 18:24:37 -0400

    guess this didn't get merged on

--- controllers/store_controller.js
+++ controllers/store_controller.js
@@ -1,4 +1,14 @@
+const mongoose = require('mongoose');
 
 exports.homePage = (req, res) => {
 	res.render('index');
+}
+
+exports.addStore = (req, res) => {
+	// res.send('it works')
+	res.render('editStore', {title: 'Add Store'})
+}
+
+exports.createStore = (req, res) => {
+	res.json(req.body);
 }
