3562e30ef77c4c16c35a6afa9c7272f474d585d8
Casey Cole <caseycole589@gmail.com>
2018-06-12 21:10:39 -0400

    saved some data to mongo

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const store_controller = require('../controllers/store_controller')
-
+const {catchErrors} = require('../handlers/errorHandlers')
 
 router.get('/', store_controller.homePage);
 router.get('/add', store_controller.addStore);
-router.post('/add', store_controller.createStore);
+router.post('/add', catchErrors(store_controller.createStore));
 
 module.exports = router;
