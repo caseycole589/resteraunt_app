af1e3453a593c3e0925c83527300ef0cc8326e1c
Casey Cole <caseycole589@gmail.com>
2018-06-15 17:58:21 -0400

    now i have a store that redirects on save and flashes success message

--- controllers/store_controller.js
+++ controllers/store_controller.js
@@ -3,15 +3,16 @@ const Store = mongoose.model('Store')
 
 exports.homePage = (req, res) => {
 	res.render('index');
+	req.flash('error', `Somthing Hamppend`);
 }
 
 exports.addStore = (req, res) => {
 	// res.send('it works')
-	res.render('editStore', {title: 'Add Store'})
+	res.render('editStore', { title: 'Add Store'})
 }
 
 exports.createStore = async (req, res) => {
-	const store = new Store(req.body);
-	await store.save(); // fires off connection to mongo db database
-	res.redirect('/');
+	const store = await (new Store(req.body)).save();// fires off connection to mongo db database
+	req.flash('success', `Successfully Created ${store.name}! Care To Leave A Review?`);
+	res.redirect(`/store/${store.slug}`);
 }
