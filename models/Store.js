ee18adbae9abeae67e98c5cb1b265faf2eaf77a1
Casey Cole <caseycole589@gmail.com>
2018-06-24 20:30:21 -0400

    feature: pictures are now uploaded resized and saved to file system

--- models/Store.js
+++ models/Store.js
@@ -4,44 +4,45 @@ mongoose.Promise = global.Promise;
 const slug = require('slugs');
 /*slug will be auto generated when somebody saves*/
 const storeSchema = new mongoose.Schema({
-	name:{
-		type:String,
-		trim: true,
-		required:'Please enter a store name' 
-	},
-	slug:String,
-	description:{
-		type:String,
-		trim: true
-	},
-	tags: [String],
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	location: {
-		type: {
-			type:String,
-			default: 'Point'
-		},
-		coordinates:[{
-			type: Number,
-			required: 'You must supply coordinates!'
-		}], 
-		address:{
-			type:String,
-			required: 'You must supply an address'
-		}	
-	}
+    name: {
+        type: String,
+        trim: true,
+        required: 'Please enter a store name'
+    },
+    slug: String,
+    description: {
+        type: String,
+        trim: true
+    },
+    tags: [String],
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    location: {
+        type: {
+            type: String,
+            default: 'Point'
+        },
+        coordinates: [{
+            type: Number,
+            required: 'You must supply coordinates!'
+        }],
+        address: {
+            type: String,
+            required: 'You must supply an address'
+        }
+    },
+    photo: String
 });
 
-storeSchema.pre('save',function(next){
-	// only needs to be ran if name is new or changed
-	if (!this.isModified('name')) {
-		return next();
-	}
-	this.slug = slug(this.name);
-	next();
-	//Todo make more resiliant so that names are guranteed to be unique
+storeSchema.pre('save', function(next) {
+    // only needs to be ran if name is new or changed
+    if (!this.isModified('name')) {
+        return next();
+    }
+    this.slug = slug(this.name);
+    next();
+    //Todo make more resiliant so that names are guranteed to be unique
 })
 module.exports = mongoose.model('Store', storeSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const store_controller = require('../controllers/store_controller')
-const {catchErrors} = require('../handlers/errorHandlers')
+const { catchErrors } = require('../handlers/errorHandlers')
 
 router.get('/', catchErrors(store_controller.getStores));
 router.get('/stores', catchErrors(store_controller.getStores));
 router.get('/add', store_controller.addStore);
-router.post('/add', catchErrors(store_controller.createStore));
-router.post('/add/:id', catchErrors(store_controller.updateStore));
+router.post('/add',
+    store_controller.upload,
+    catchErrors(store_controller.resize),
+    catchErrors(store_controller.createStore)
+);
+router.post('/add/:id',
+    store_controller.upload,
+    catchErrors(store_controller.resize),
+    catchErrors(store_controller.updateStore)
+);
 router.get('/stores/:id/edit', catchErrors(store_controller.editStore));
 module.exports = router;
