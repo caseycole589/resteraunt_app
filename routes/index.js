4ed2663fb5ee96fb9cd72d9b6a57931e718af83b
Casey Cole <caseycole589@gmail.com>
2018-07-15 20:05:40 -0400

    feature: got the login route done

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const store_controller = require('../controllers/store_controller')
 const { catchErrors } = require('../handlers/errorHandlers')
+const user_controller = require('../controllers/user_controller');
 
 router.get('/stores', catchErrors(store_controller.getStores));
 router.get('/add', store_controller.addStore);
@@ -21,4 +22,7 @@ router.get('/store/:slug', catchErrors(store_controller.getStoreBySlug))
 
 router.get('/tags', catchErrors(store_controller.getStoresByTag));
 router.get('/tags/:tag', catchErrors(store_controller.getStoresByTag));
+
+router.get('/login', user_controller.loginForm)
+// router.post('/login', user_controller.)
 module.exports = router;
