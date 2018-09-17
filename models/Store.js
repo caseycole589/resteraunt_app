0b90b2f38c4a24bc3c684a549647dc311ae92765
Casey Cole <caseycole589@gmail.com>
2018-09-17 16:23:58 -0400

    feature: Store aggregation
    Stores top 10 now aggregrated and if user is not logged in shows the number of stores on top of card

--- models/Store.js
+++ models/Store.js
@@ -63,7 +63,7 @@ storeSchema.pre('save', async function(next) {
     //^startof string matchest this.slug at start
     //$ end matches $t in eat but not the t in eater
     //() remembers match
-    //-[0-9]* mattch -digits 0 through 9, 0 0r more times 
+    //-[0-9]* match -digits 0 through 9, 0 0r more times 
     //question mark means optional
     const slugRegex = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i')
     const storesWithSlug = await this.constructor.find({ slug: slugRegex });
@@ -86,6 +86,33 @@ storeSchema.statics.getTagsList = function() {
         { $sort: { count: -1 } }
     ]);
 }
+
+storeSchema.statics.getTopStores = function() {
+    console.log('what')
+    return this.aggregate([
+        //Lookup stores and populate there reviews
+        //mongo takes model name lowercase it and add s hince from: reviews
+        { $lookup: { from: 'reviews', localField: '_id', foreignField: 'store', as: 'reviews' } },
+        //filter for only items that have 2 or more reviews
+        { $match: { 'reviews.1': { $exists: true } } },
+        //add the average reviews field dollar sign on $reviews.rating means the field is coming
+        //from data being piped in
+        {
+            $project: {
+                photo: '$$ROOT.photo',
+                name: '$$ROOT.name',
+                reviews: '$$ROOT.reviews',
+                slug: '$$ROOT.slug',
+                averageRating: { $avg: '$reviews.rating' }
+            }
+        },
+        //sort if by our new field highes reviews first
+        { $sort: { averageRating: -1 } },
+        //limit to at most 10
+        { $limit: 10 }
+    ]);
+}
+
 //dind reviews where stored id  == reviews store prop
 storeSchema.virtual('reviews', {
     ref: "Review",
@@ -93,4 +120,11 @@ storeSchema.virtual('reviews', {
     foreignField: "store" //which fiedld on review
 })
 
+function autopopulate(next) {
+    this.populate('reviews');
+    next();
+}
+
+storeSchema.pre('find', autopopulate)
+storeSchema.pre('findOne', autopopulate)
 module.exports = mongoose.model('Store', storeSchema);
